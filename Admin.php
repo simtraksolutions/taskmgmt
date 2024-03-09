9d5304ac8ca771e566f599cfb630ab2517d721c3
sumitth456 <140262337+sumitth456@users.noreply.github.com>
2024-03-09 18:56:26 +0530

    Update Admin.php

--- Admin.php
+++ Admin.php
@@ -11,69 +11,135 @@ if (isset($_POST['logout'])) {
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Admin-Page</title>
-
-
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <style>
-  .navbar-brand {
-    color: white;
-    font-size: 2rem;
-    font-weight: 600;
-  }
+    .navbar-brand {
+      color: white;
+      font-size: 2rem;
+      font-weight: 600;
+    }
 
-  .nav-item a {
-    color: white;
-    font-size: 1.3rem;
-    font-weight: 500;
+  
+    .nav-item a {
+      color: white;
+      font-size: 1.3rem;
+      font-weight: 500;
+  
+    }
+  
+    .nav-item button {
+      font-weight: 500;
+      margin-top: 5px;
+      margin-left: 15px;
+    }
 
-  }
+    .Task-section{
+      
+      height: 70vh;
+      width: 100%;
+      margin-top: 15px;
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+    }
 
-  .nav-item button {
-    font-weight: 500;
-    margin-top: 5px;
-    margin-left: 15px;
-  }
-</style>
+    #my-tasks, #mem-task{
+      border-radius: 20px;
+     
+    }
 
+   
+
+    #my-tasks{
+   
+      height: 50vh;
+      width: 40%;
+      background: #1844bf;
+      text-align: center;
+      scale: 1;
+      transition: all ease 0.5s;
+   
+    }
+    #my-tasks:hover{
+      scale: 1.1;
+    }
+
+
+    #mem-task{
+      height: 50vh;
+      width: 40%;
+      background: rgb(152, 73, 231);
+      scale: 1;
+      transition: all ease 0.5s;
+    }
+
+    #mem-task:hover{
+      scale: 1.1;
+    }
+
+    #span-text{
+      height: 25vh;
+      width: 40%;
+      margin: auto;
+      margin-top: 100px;
+      text-align: left;
+      color: whitesmoke;
+      border-left: 3px solid white;
+    }
+
+
+  .Task-section a{
+      text-decoration: none;
+    }
+
+
+    #span-text h1{
+      font-size: 3rem;
+      font-weight: 600;
+      padding-left: 15px;
+
+    }
+
+    #soan-text :hover{
+      color: black;
+    }
+  </style>
+  
 <body>
-  <main>
     <nav class="navbar navbar-expand-lg bg-primary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Admin's Page</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link " href="adore.php">Approval Page</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="task.php">Task Form</a>
-            </li>
-            <li class="nav-item">
-              <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                <button class=" btn btn-light" type="submit" name="logout">Logout</button>
-              </form>
-
-            </li>
-
-          </ul>
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Admin's Page</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link " href="adore.php">Approval Page</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="task.php">Task Form</a>
+              </li>
+              <li class="nav-item">
+                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                  <button class=" btn btn-light" type="submit" name="logout">Logout</button>
+                </form>
+  
+              </li>
+  
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-    <?php
+      </nav>
+      <?php
 include 'connect.php'; // Assuming this file contains your database connection
 
 $query = "SELECT * FROM tform";
@@ -83,39 +149,33 @@ if (!$result) {
     die("Query failed: " . mysqli_error($con));
 }
 ?>
-    <table class="table table-striped table-bordered">
-  <thead>
-    <tr>
-      
-      <th scope="col">Task Name</th>
-      <th scope="col">Task Given By</th>
-      <th scope="col">Task Given To</th>
-      <th scope="col">Date Of Allocation</th>
-      <th scope="col">Task Status</th>
-    </tr>
-  </thead>
-  <tbody>
-  <?php
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<tr>";
-                 
-                    echo "<td>{$row['name']}</td>";
-                    echo "<td>{$row['email']}</td>";
-                    echo "<td>{$row['recipient_email']}</td>";
-                    
-                    echo "<td>{$row['currentDate']}</td>";
-                    echo "<td>{$row['status']}</td>";
-                    echo "</tr>";
-                }
-                ?>
-            </tbody>
-</table>
-  </main>
+<main>
+<section class="Task-section">
+<div id="my-tasks">
+  <a href="adore.php">
+  <div id="span-text">
+  <span><h1>MY</h1></span>
+  <span><h1>TASKS</h1></span>
+ </div>
 
+  </a>
+ 
+</div>
+<div id="mem-task">
+<a href="Admintable.php">
+  <div id="span-text">
+  <span><h1>MEMBER'S</h1></span>
+  <span><h1>TASKS</h1></span>
+ </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
+  </a>
+</div>
+
+</section>
+</main>
+
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>    
 </body>
-
 </html>
